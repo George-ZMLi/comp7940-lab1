afd256562cb775c9807262d40c17f88522b59ab6
George_ZMLi <George_zmli@163.com>
2023-01-17 20:57:40 +0800

    Second commit

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
 def main():
     print('Hello World')
+    
+def print_factor(x):
+    for i in range(x+1):
+        if i != 0 and x % i == 0:
+            print(i, x/i)
 
 if __name__ == '__main__':
     main()
+    l = [52633,8137,1024,999]
+    for j in l:
+        print_factor(j)
